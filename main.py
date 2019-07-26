79fbe581fe1cfc0e58a83e77d74b8604d6688c1c
Mutluhan Üzmez <mutluhanuzmez@gmail.com>
2019-07-26 19:24:22 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+window = Tk()
+window.state('zoomed')
+window.configure(bg = 'green4')
+
+def drag(event):
+    event.widget.place(x=event.x_root, y=event.y_root,anchor=CENTER)
+    print((event.x, event.y))
+
+card = Canvas(window, width=74, height=97, bg='blue')
+card.place(x=300, y=600,anchor=CENTER)
+card.bind("<B1-Motion>", drag)
+
+another_card = Canvas(window, width=74, height=97, bg='red')
+another_card.place(x=600, y=600,anchor=CENTER)
+another_card.bind("<B1-Motion>", drag)
+
+window.mainloop()
