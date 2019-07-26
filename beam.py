e8ac3ee601ba57aa810ec1ddebc9fdd448abf2f1
alitatici <alitalha2a2@gmail.com>
2019-07-26 21:23:42 +0300

    classes are added

--- beam.py
+++ beam.py
@@ -1,58 +1,142 @@
-class VerticalBeam:
-    length=0
-    thickness=0
-    centerX=0
+class Concrete:
+    concreteType=0
+    fck=0
+    fcd=0
+    fctd=0
+    concreteElasticityModulus=0
+    k1=0
 
 # parameterized constructor 
-    def __init__(self, length, thickness, centerX): 
-        self.length = length
-        self.thickness = thickness
-        self.centerX = centerX
-      
-    def display(self): 
-        print("____________________________________")
-        print("VerticalBeam")
-        print("Length = " + str(self.length)) 
-        print("Thickness = " + str(self.thickness)) 
-        print("CenterX = " + str(self.centerX)) 
-  
-    def calculate(self): 
-        self.centerX = self.length + self.thickness 
+    def __init__(self, concreteType): 
+        self.concreteType = concreteType
+        if concreteType == "C20":
+            self.fck=20
+            self.fcd=13.3
+            self.fctd=1.04
+            self.concreteElasticityModulus=28000
+            self.k1=0.85
+        elif concreteType == "C25":
+            self.fck=25
+            self.fcd=16.7
+            self.fctd=1.17
+            self.concreteElasticityModulus=30000
+            self.k1=0.85
+        elif concreteType == "C30":
+            self.fck=30
+            self.fcd=20
+            self.fctd=1.28
+            self.concreteElasticityModulus=32000
+            self.k1=0.82
+        elif concreteType == "C35":
+            self.fck=35
+            self.fcd=23.3
+            self.fctd=1.38
+            self.concreteElasticityModulus=33000
+            self.k1=0.79
+        elif concreteType == "C40":
+            self.fck=40
+            self.fcd=26.7
+            self.fctd=1.48
+            self.concreteElasticityModulus=34000
+            self.k1=0.76
+        elif concreteType == "C45":
+            self.fck=45
+            self.fcd=30
+            self.fctd=1.57
+            self.concreteElasticityModulus=36000
+            self.k1=0.73
+        elif concreteType == "C50":
+            self.fck=50
+            self.fcd=33.3
+            self.fctd=1.65
+            self.concreteElasticityModulus=37000
+            self.k1=0.70
 
-
-verticalBeam=VerticalBeam(30,30,30)
-verticalBeam.display()
-
-class HorizontalBeam:
-    length=0
-    thickness=0
-    centerY=0
+class ReinforcementSteel:
+    reinforcementSteelType=0
+    fyd=0
 
 # parameterized constructor 
-    def __init__(self, length, thickness, centerX): 
-        self.length = length
+    def __init__(self, reinforcementSteelType): 
+        self.reinforcementSteelType = reinforcementSteelType
+        if reinforcementSteelType == "S220":
+            self.fyd=191
+        elif reinforcementSteelType == "S420":
+            self.fyd=365
+        elif reinforcementSteelType == "S500":
+            self.fyd=435
+
+class Hatil:
+    thickness=0
+    location=0
+    linearEquivalentEarthquakeLoad=0
+    maximumMoment=0
+    necessaryReinforcementArea=0
+    maximumReinforcementArea=0
+    minimumReinforcementArea=0
+    maximumShearForce=0
+    necessaryStirrup=0
+    deflection=0
+    def __init__(self, thickness): 
         self.thickness = thickness
-        self.centerY = centerX
-      
-    def display(self):
-        print("____________________________________")
-        print("HorizontalBeam")
-        print("Length = " + str(self.length)) 
-        print("Thickness = " + str(self.thickness)) 
-        print("CenterY = " + str(self.centerY)) 
-  
-    def calculate(self): 
-        self.centerX = self.length + self.thickness 
 
+class VerticalHatil(Hatil):
+    length=0
+    pointLoadFromHorizontalHatils=0
 
-horizontalBeam1=HorizontalBeam(30,30,30)
-horizontalBeam1.display()
+class HorizontalHatil(Hatil):
+    leftLength=0
+    rightLength=0
+    linearLoadFromWall=0
 
-horizontalBeam2=HorizontalBeam(30,30,30)
-horizontalBeam2.display()
+class Wall:
+    density=0
+    thickness=0
+    def __init__(self, thickness, density): 
+        self.thickness = thickness
+        self.density = density
 
+class Plaster:
+    density=0
+    thickness=0
+    def __init__(self, thickness, density): 
+        self.thickness = thickness
+        self.density = density
 
-print(verticalBeam.length*horizontalBeam1.length) 
+class ReinforcedConcreteDensity:
+    reinforcedConcreteDensity=0
+    def __init__(self): 
+        self.reinforcedConcreteDensity = 2500
+    def setDensity(self, newDensity):
+        self.reinforcedConcreteDensity = newDensity
 
+class ConcreteCover:
+    coverThickness=0
+    def __init__(self, coverThickness):
+        self.coverThickness =coverThickness
+
+class HeightParameter:
+    heightFromBasement=0
+    heightMax=0
+    heightRatio=0
+    def __init__(self, heightFromBasement, heightMax):
+        self.heightFromBasement=heightFromBasement
+        self.heightMax=heightMax
+        if heightFromBasement/heightMax < 0:
+            self.heightRatio = 1
+        else:
+            self.heightRatio = heightFromBasement/heightMax
+
+class Earthquake:
+    A0=0
+    I=0
+    def __init__(self, A0, I):
+        self.A0 = A0
+        self.I = I
+
+class Calculated:
+    wallLinearWeight = 0
+    wallWeightPerUnitArea = 0 
+    ratioOfSpans = 0
 
 
